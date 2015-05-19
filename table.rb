f99d473aabfef813ea7597e0179b6b96da3a783a
John Peterson <jlpeterson829@gmail.com>
2015-05-19 13:12:16 -0700

    Make class for prime mult table, default initialize to 10

--- table.rb
+++ table.rb
@@ -1,41 +1,49 @@
-# get list of first n primes
-def listPrimes n
-  sieve = []
-  current = 2
-  until sieve.length == n 
-    if isPrime current
-      sieve.push(current)
-      current += 1
-    else 
-      current += 1
+class PrimeMultiplicationTable 
+  
+  def initialize n=10
+    @n = n
+  end
+
+  # make multiplication table
+  def makeTable 
+    x = y = listPrimes @n
+    print '     '
+    x.each {|i| print "%-3d  " % i}
+    print "\n     "
+    x.each {|i| print '---- '}
+    print "\n"
+
+    y.each do |j|
+      print "%-3d| " % j
+      x.each {|i| print "%-3d  " % (i*j)}
+      print "\n"
     end
   end
-  sieve
-end
 
-# check if a given number is prime
-def isPrime num
-  divider = false
-  for i in 2 .. Math.sqrt(num)
-    divider = ((num % i) == 0) || divider
+  # get list of first n primes
+  def listPrimes n
+    sieve = []
+    current = 2
+    until sieve.length == n 
+      if isPrime current
+        sieve.push(current)
+        current += 1
+      else 
+        current += 1
+      end
+    end
+    sieve
   end
-  not divider
-end
 
-# make multiplication table
-def primeTable n=10
-  x = y = listPrimes n
-  print '     '
-  x.each {|i| print "%-3d  " % i}
-  print "\n     "
-  x.each {|i| print '---- '}
-  print "\n"
-
-  y.each do |j|
-    print "%-3d| " % j
-    x.each {|i| print "%-3d  " % (i*j)}
-    print "\n"
+  # check if a given number is prime
+  def isPrime num
+    divider = false
+    for i in 2 .. Math.sqrt(num)
+      divider = ((num % i) == 0) || divider
+    end
+    not divider
   end
 end
 
-primeTable 4
+# table = PrimeMultiplicationTable.new
+# table.makeTable
